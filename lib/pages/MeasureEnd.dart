20afe52718a9101ec10782f29f050bf92a302f9c
arakisota <soutaraki0126@gmail.com>
2023-07-02 15:43:46 +0900

    計測終了画面を作成

--- lib/pages/MeasureEnd.dart
+++ lib/pages/MeasureEnd.dart
@@ -1,6 +1,49 @@
 import 'package:benesse_hackathon/constants/route.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'My page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(20), // マージンを設定
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('/Users/sotaaraki/StudioProjects/fix_ranking/lib/finish_page.png'), // 画像ファイルのパス
+            fit: BoxFit.cover, // 画像をウィジェットに合わせて拡大縮小
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
 class MeasureEnd extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
