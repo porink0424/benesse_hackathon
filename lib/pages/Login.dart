638d2b09675f012838e7f0c8c9c625ae8f2adf8e
shino <106672783+shino-shino@users.noreply.github.com>
2023-07-02 12:02:17 +0900

    Update Login.dart
    feat: ログイン日数表示画面追加

--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -5,23 +5,106 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text(
-          'Login',
-          textDirection: TextDirection.ltr,
-        )),
-        body: Row(
-          children: [
-            TextButton(
-              child: Text(
-                'Go to ${ROUTE.MYPAGE}',
-                textDirection: TextDirection.ltr,
-              ),
-              onPressed: () {
-                Navigator.pushNamed(context, ROUTE.MYPAGE);
-              },
+      appBar: AppBar(
+        title: const Text('Log in calendar'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 80,
+            margin: EdgeInsets.only(top: 20, left: 20 ,right: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.black, width: 2),
             ),
-          ],
-        ));
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'ログイン記録',
+                  // textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 160,
+            margin: EdgeInsets.only(top: 20, left: 20 ,right: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.black, width: 2),
+            ),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '連続ログイン記録',
+                  // textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  '37日目',
+                  // textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 60,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 160,
+            margin: EdgeInsets.only(top: 20, left: 20 ,right: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.black, width: 2),
+            ),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '連続ログイン記録',
+                  // textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  '52日目',
+                  // textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 60,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, ROUTE.MYPAGE);
+            },
+            child: Text("Tap to next"),
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 30),
+              foregroundColor: Colors.blue, // foreground
+              fixedSize: Size(200, 100),
+              alignment: Alignment.topCenter,
+            )),
+        ],
+      ),
+    );
   }
 }
