c03e825408df8b9c6e001853bf0577e7d0d5ae5e
guy8418 <guy84188418@gmail.com>
2023-07-02 12:01:44 +0900

    ストップウォッチの最初の画面を作成

--- lib/pages/MeasureStart.dart
+++ lib/pages/MeasureStart.dart
@@ -4,24 +4,111 @@ import 'package:flutter/material.dart';
 class MeasureStart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    final double deviceWidth = MediaQuery.of(context).size.width;
+
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
-            title: Text(
-          'MeasureStart',
+            title: const Text(
+          'Timer',
           textDirection: TextDirection.ltr,
         )),
-        body: Row(
-          children: [
-            TextButton(
-              child: Text(
-                'Go to ${ROUTE.MEASURE}',
-                textDirection: TextDirection.ltr,
+
+        body: Center(
+          child: Container(
+            child: Column(children: [
+              Container(
+                child: Row(
+                  children: [
+                    Container(
+                        child: TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.red,
+                              shape: CircleBorder()),
+                          child: Text(
+                            '国',
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(fontSize: 10, color: Colors.white),
+                          ),
+                        ),
+                        width: deviceWidth / 5),
+                    Container(
+                        child: TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.orange,
+                              shape: CircleBorder()),
+                          child: Text(
+                            '社',
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(fontSize: 10, color: Colors.white),
+                          ),
+                        ),
+                        width: deviceWidth / 5),
+                    Container(
+                        child: TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.yellow,
+                              shape: CircleBorder()),
+                          child: Text(
+                            '英',
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(fontSize: 10, color: Colors.white),
+                          ),
+                        ),
+                        width: deviceWidth / 5),
+                    Container(
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            shape: CircleBorder()),
+                        child: Text(
+                          '社',
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(fontSize: 10, color: Colors.white),
+                        ),
+                      ),
+                      width: deviceWidth / 5,
+                    ),
+                    Container(
+                        child: TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              shape: CircleBorder()),
+                          child: Text(
+                            '数',
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(fontSize: 10, color: Colors.white),
+                          ),
+                        ),
+                        width: deviceWidth / 5),
+                  ],
+                ),
               ),
-              onPressed: () {
-                Navigator.pushNamed(context, ROUTE.MEASURE);
-              },
-            ),
-          ],
-        ));
+              Column(
+                children: [
+                  TextButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.teal),
+                    ),
+                    child: const Text(
+                      '学習開始',
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(fontSize: 10, color: Colors.white),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ]),
+          ),
+        ),
+      ),
+    );
   }
 }
