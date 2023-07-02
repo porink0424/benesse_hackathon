96a68abdf52d920baf9dbcf61e2a30715c360789
Daichi Kato <daichi5967@docomo.ne.jp>
2023-07-02 13:07:38 +0900

    スタイルの調整

--- lib/pages/MeasureStart.dart
+++ lib/pages/MeasureStart.dart
@@ -6,92 +6,91 @@ class MeasureStart extends StatelessWidget {
   Widget build(BuildContext context) {
     final double deviceWidth = MediaQuery.of(context).size.width;
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-            title: const Text(
-          'Timer',
-          textDirection: TextDirection.ltr,
-        )),
-
-        body: Center(
-          child: Container(
-            child: Column(children: [
-              Container(
-                child: Row(
-                  children: [
-                    Container(
-                        child: TextButton(
-                          onPressed: () {},
-                          style: TextButton.styleFrom(
-                              backgroundColor: Colors.red,
-                              shape: CircleBorder()),
-                          child: Text(
-                            '国',
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(fontSize: 10, color: Colors.white),
-                          ),
-                        ),
-                        width: deviceWidth / 5),
-                    Container(
-                        child: TextButton(
-                          onPressed: () {},
-                          style: TextButton.styleFrom(
-                              backgroundColor: Colors.orange,
-                              shape: CircleBorder()),
-                          child: Text(
-                            '社',
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(fontSize: 10, color: Colors.white),
-                          ),
-                        ),
-                        width: deviceWidth / 5),
-                    Container(
-                        child: TextButton(
-                          onPressed: () {},
-                          style: TextButton.styleFrom(
-                              backgroundColor: Colors.yellow,
-                              shape: CircleBorder()),
-                          child: Text(
-                            '英',
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(fontSize: 10, color: Colors.white),
-                          ),
-                        ),
-                        width: deviceWidth / 5),
-                    Container(
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+        '学習時間計測',
+        textDirection: TextDirection.ltr,
+      )),
+      body: Center(
+        child: Container(
+          height: 200,
+          child: Column(children: [
+            Container(
+              child: Row(
+                children: [
+                  Container(
                       child: TextButton(
                         onPressed: () {},
                         style: TextButton.styleFrom(
-                            backgroundColor: Colors.green,
+                            backgroundColor: Colors.red, shape: CircleBorder()),
+                        child: Text(
+                          '国',
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(fontSize: 24, color: Colors.white),
+                        ),
+                      ),
+                      width: deviceWidth / 5),
+                  Container(
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                            backgroundColor: Colors.orange,
                             shape: CircleBorder()),
                         child: Text(
                           '社',
                           textDirection: TextDirection.ltr,
-                          style: TextStyle(fontSize: 10, color: Colors.white),
+                          style: TextStyle(fontSize: 24, color: Colors.white),
                         ),
                       ),
-                      width: deviceWidth / 5,
-                    ),
-                    Container(
-                        child: TextButton(
-                          onPressed: () {},
-                          style: TextButton.styleFrom(
-                              backgroundColor: Colors.blue,
-                              shape: CircleBorder()),
-                          child: Text(
-                            '数',
-                            textDirection: TextDirection.ltr,
-                            style: TextStyle(fontSize: 10, color: Colors.white),
-                          ),
+                      width: deviceWidth / 5),
+                  Container(
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                            backgroundColor: Colors.yellow,
+                            shape: CircleBorder()),
+                        child: Text(
+                          '英',
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(fontSize: 24, color: Colors.white),
                         ),
-                        width: deviceWidth / 5),
-                  ],
-                ),
+                      ),
+                      width: deviceWidth / 5),
+                  Container(
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                          backgroundColor: Colors.green, shape: CircleBorder()),
+                      child: Text(
+                        '社',
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(fontSize: 24, color: Colors.white),
+                      ),
+                    ),
+                    width: deviceWidth / 5,
+                  ),
+                  Container(
+                      child: TextButton(
+                        onPressed: () {},
+                        style: TextButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            shape: CircleBorder()),
+                        child: Text(
+                          '数',
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(fontSize: 24, color: Colors.white),
+                        ),
+                      ),
+                      width: deviceWidth / 5),
+                ],
               ),
-              Column(
-                children: [
-                  TextButton(
+            ),
+            Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 30.0),
+                  child: TextButton(
                     style: ButtonStyle(
                       backgroundColor:
                           MaterialStateProperty.all<Color>(Colors.teal),
@@ -99,14 +98,16 @@ class MeasureStart extends StatelessWidget {
                     child: const Text(
                       '学習開始',
                       textDirection: TextDirection.ltr,
-                      style: TextStyle(fontSize: 10, color: Colors.white),
+                      style: TextStyle(fontSize: 30, color: Colors.white),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.pushNamed(context, ROUTE.MEASURE);
+                    },
                   ),
-                ],
-              ),
-            ]),
-          ),
+                )
+              ],
+            ),
+          ]),
         ),
       ),
     );
